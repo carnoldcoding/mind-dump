e7bbc2b2bee528305e8f96914210015bdf866d8c
Logia <logia@camcode.dev>
2025-08-10 14:24:37 -0400

    GameDetails working on mobile

--- src/pages/Games/GameDetail.tsx
+++ src/pages/Games/GameDetail.tsx
@@ -52,32 +52,35 @@ const GameDetail = () => {
         if (!data) return <div>Game not found</div>; 
 
         return (
-            <article className="w-5xl h-130 bg-nier-100 mt-5 relative">
+            <article className="md:w-5xl md:h-130 bg-nier-100 mt-5 relative">
                 
                 <div className="h-10 w-full bg-nier-150 flex items-center justify-between px-5">
                     <h3 className="text-nier-text-dark text-xl">{data.title}</h3>
                     <div onClick={handleClose} className="text-3xl relative cursor-pointer">×</div>
                 </div>
-                <div className="p-4 flex">
-                    <div className="min-w-80 h-112 bg-cover bg-center" style={{backgroundImage:`url(${data.image_path})`}}></div>
+                <div className="py-4 md:p-4 flex-col flex md:flex-row">
+                    <div className="md:min-w-80 md:h-112 bg-cover bg-center" style={{backgroundImage:`url(${data.image_path})`}}></div>
                     <div className="absolute bottom-2 h-[1px] w-[calc(100%-35px)] bg-nier-150"></div>
                     <div className="absolute top-12 h-[1px] w-[calc(100%-35px)] bg-nier-150"></div>
 
                     <div className="px-5 flex flex-col gap-5 overflow-y-scroll h-112">
+                        <div className="flex items-start gap-3 md:gap-0 w-full justify-between">
+                            <div className="h-50 basis-1/2 md:hidden bg-cover bg-center" style={{backgroundImage:`url(${data.image_path})`}}></div>
+                            
+                            <div className="flex gap-2 md:gap-0 flex-col items-start md:flex-row md:justify-between w-full basis-1/2 md:basis-auto">
+                                <div className="flex items-center justify-center bg-nier-dark h-14 w-14">
+                                    <p className="text-nier-text-light text-2xl leading-none">{data.rating.toString()}</p>
+                                </div>
 
-                        <div className="flex justify-between w-full">
-                            <div className="flex items-center justify-center bg-nier-dark h-14 w-14">
-                                <p className="text-nier-text-light text-2xl leading-none">{data.rating.toString()}</p>
-                            </div>
-
-                            <div className="flex gap-2 self-end">
-                                {data.genres.map((genre : string)=>{
-                                    return (
-                                        <div className="px-2 py-1 bg-nier-150/80 flex justify-center items-center">
-                                            <p>{genre}</p>
-                                        </div>
-                                    )
-                                })}
+                                <div className="flex flex-wrap gap-2">
+                                    {data.genres.map((genre : string)=>{
+                                        return (
+                                            <div className="p-1 md:px-2 md:py-1 bg-nier-150/80 flex justify-center items-center">
+                                                <p className="text-xs md:text-sm">{genre}</p>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
                             </div>
                         </div>
                         <div>
